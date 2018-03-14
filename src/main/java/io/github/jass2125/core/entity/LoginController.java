2a32f3032a76271f0d750ac64c4b272e592947ba
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-14 16:58:07 -0300

    create login

---
+++ src/main/java/io/github/jass2125/core/entity/LoginController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.jass2125.core.entity;
+
+import java.io.Serializable;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ * @since Mar 14, 2018 4:45:39 PM
+ */
+@Named
+public class LoginController implements Serializable {
+
+    @Inject
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void print() {
+        System.out.println("Enviar");
+    }
+}
--- src/main/java/io/github/jass2125/core/entity/User.java
+++ src/main/java/io/github/jass2125/core/entity/User.java
@@ -6,11 +6,70 @@
 package io.github.jass2125.core.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * @author Anderson Souza <jair_anderson_bs@hotmail.com>
  * @since Mar 14, 2018 4:06:04 PM
  */
 public class User implements Serializable {
+
+    private Long id;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "email=" + email + ", password=" + password + '}';
+    }
 
 }
