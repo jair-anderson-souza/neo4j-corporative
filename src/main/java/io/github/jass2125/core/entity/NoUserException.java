b0380299b0b62a68c1749da290c33f4716205b09
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-15 16:06:37 -0300

    create runtimeexception from user not found

---
+++ src/main/java/io/github/jass2125/core/entity/NoUserException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.jass2125.core.entity;
+
+/**
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ * @since Mar 15, 2018 3:48:12 PM
+ */
+public class NoUserException extends RuntimeException {
+
+    public NoUserException(Exception e, String msg) {
+    }
+
+}
