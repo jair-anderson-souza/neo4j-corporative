c6dc1a077116974a4f6cfab201a8e185246c5546
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-15 16:07:59 -0300

    create interface userdao

--- src/main/java/io/github/jass2125/core/entity/UserDao.java
+++ src/main/java/io/github/jass2125/core/entity/UserDao.java
@@ -5,23 +5,11 @@
  */
 package io.github.jass2125.core.entity;
 
-import java.io.Serializable;
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 /**
  * @author Anderson Souza <jair_anderson_bs@hotmail.com>
- * @since Mar 15, 2018 1:27:41 PM
+ * @since Mar 15, 2018 3:46:14 PM
  */
-@Stateless
-public class UserDao implements Serializable {
+public interface UserDao {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    public UserPrincipal findUserByEmailAndPassword(UserPrincipal user) {
-        System.out.println(user);
-        return (UserPrincipal) em.createQuery("SELECT U FROM UserPrincipal U WHERE U.account.email = :email AND U.account.password = :password").setParameter("email", user.getEmail()).setParameter("password", user.getPassword()).getSingleResult();
-    }
+    public UserPrincipal findUserByEmailAndPassword(UserPrincipal user);
 }
