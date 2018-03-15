f1c0cb5014690e3cf941d44fb339bc361502ae9a
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-15 16:49:06 -0300

    connection neo4j

---
+++ src/main/java/io/github/jass2125/core/entity/ConnectionImp.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.jass2125.core.entity;
+
+import javax.ejb.Stateless;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+
+/**
+ * @author Anderson Souza <jair_anderson_bs@hotmail.com>
+ * @since Mar 15, 2018 4:20:33 PM
+ */
+@Stateless
+public class ConnectionImp implements Connection {
+
+    @Override
+    public Driver openConnection() {
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "123"));
+        return driver;
+//        try (Session session = driver.session()) {
+//            //            StatementResult result =
+//            StatementResult result = session.
+//                    run("CREATE (p:Produto{codigo : $codigo, descricao : $descricao, preco : $preco}) RETURN id(p) as id",
+//                            Values.parameters("codigo", 2, "descricao", "Feijão", "preco", 5.50f));
+//            System.out.println(result.single().get("id"));
+//        } finally {
+//            driver.close();
+    }
+}
