8e3b79d7b76d4b950fda9c1b0c04a768f800aa39
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-15 21:05:13 -0300

    class Post

--- src/main/java/io/github/jass2125/core/entity/Post.java
+++ src/main/java/io/github/jass2125/core/entity/Post.java
@@ -6,11 +6,70 @@
 package io.github.jass2125.core.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * @author Anderson Souza <jair_anderson_bs@hotmail.com>
  * @since Mar 15, 2018 4:15:50 PM
  */
 public class Post implements Serializable {
+
+    private Long id;
+    private String comment;
+    private User user;
+
+    public Post() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Post other = (Post) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" + "id=" + id + ", comment=" + comment + '}';
+    }
 
 }
